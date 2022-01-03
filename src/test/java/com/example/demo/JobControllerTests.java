d04a63db3a8a6c6eae2b0eebc78a73f0455c681c
deshanwattegama <deshanshehantha@gmail.com>
2022-01-03 20:35:49 +0530

    h2 db for tests

--- src/test/java/com/example/demo/JobControllerTests.java
+++ src/test/java/com/example/demo/JobControllerTests.java
@@ -4,8 +4,10 @@ import com.example.demo.controllers.JobController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class JobControllerTests {
 
     @Autowired
